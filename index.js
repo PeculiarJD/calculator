7217639174e5149af0b0f2426eb19fa724399d55
Peculiar <154705792+PeculiarJD@users.noreply.github.com>
2024-06-09 08:12:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("display");
+
+function handleClick(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = " "
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
